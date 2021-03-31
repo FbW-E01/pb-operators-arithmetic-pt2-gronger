c17032360941b58e565b1923738fd9a6cbe8104d
Joseph Grainger <grrainger7@gmail.com>
2021-03-31 10:05:02 +0200

    Not finished

---
+++ lol.js
@@ -0,0 +1,48 @@
+// 01
+const theaterIncome = 6450;
+const ticket = 15;
+console.log(theaterIncome / ticket);
+
+// 02
+const weeklyIncome = 500;
+const monthsInYear = 12;
+console.log(weeklyIncome * monthsInYear);
+
+// 03
+
+
+// 04
+const squareSideLength = 4.75;
+const numberOfSides = 4;
+console.log(squareSideLength * numberOfSides);
+
+// 05
+const triangleSideLengthOne = 5;
+const triangleSideLengthTwo = 6;
+const triangleSideLengthThree = 7;
+console.log(triangleSideLengthOne + triangleSideLengthTwo + triangleSideLengthThree);
+
+// 06
+const lengthOfSideOfSquare = 5;
+console.log(lengthOfSideOfSquare * lengthOfSideOfSquare);
+
+// 07
+
+
+// 08
+const cubeSideLength = 9;
+console.log(cubeSideLength * cubeSideLength *   cubeSideLength);
+
+// 09
+const billOne = 22.35;
+const billTwo = 26.67;
+const billThree = 35.92;
+
+// 10
+
+
+// 11
+
+// 12
+
+// 13
